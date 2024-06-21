ea95fd3c608b0b70a47bdff39053d7e7bb996e36
unknown <muhammad-wasi-2112>
2024-06-22 00:20:21 +0500

    Word Counter

---
+++ index.js
@@ -0,0 +1,14 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+console.log(chalk.grey.bold(`---------${chalk.magentaBright.bold("Word Counter")}---------`));
+do {
+    let answers = await inquirer.prompt([{
+            name: "Sentence",
+            type: "input",
+            message: "Enter your Sentence to count the words",
+        }]);
+    const word = answers.Sentence.trim().split(" ");
+    console.log(chalk.cyanBright.bold(`${word}`));
+    console.log(chalk.grey.bold(`Your sentence word count is ${chalk.yellowBright.bold(word.length)}`));
+} while (true);
